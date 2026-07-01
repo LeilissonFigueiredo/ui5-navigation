2d330b08c295b777d207a1004fcf13be222ce6d5
LeilissonFigueiredo <leilisson.figueiredo@gmail.com>
2026-07-01 10:41:57 -0300

    Step 3: Catch Invalid Hases

---
+++ webapp/controller/NotFound.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],(Controller) => {
+    "use strict";
+    const NotFound = Controller.extend("sap.ui.demo.nav.controller.NotFound", {
+        onInit(){
+
+        }
+    });
+
+    return NotFound;
+
+})
