ea278a932e528f97a1d7832bb17c088da27b3b90
LeilissonFigueiredo <leilisson.figueiredo@gmail.com>
2026-07-01 21:21:38 -0300

    Step 7: Navigate to Routes with Mandatory Parameters

---
+++ webapp/controller/employee/Employee.controller.js
@@ -0,0 +1,41 @@
+sap.ui.define([
+    "sap/ui/demo/nav/controller/BaseController",
+
+],(BaseController) => {
+    "use strict";
+
+    const Employee = BaseController.extend("sap.ui.demo.nav.controller.employee.Employee", {
+        onInit(){
+            const router = this.getRouter();
+            router.getRoute("employee").attachPatternMatched(this._onObjectMatched, this)
+        },
+
+        _onObjectMatched(oEvent){
+            const eventArgs = oEvent.getParameter("arguments");
+            const view = this.getView();
+            view.bindElement({
+                path: "/Employees("+ eventArgs.employeeID + ")",
+                events: {
+                    change: this._onBindingChange.bind(this),
+                    dataRequested: () => {
+                        view.setBusy(true);
+                    },
+                    dataReceived: () => {
+                        view.setBusy(false);
+                    }
+                },
+                model: "employees"
+            });  
+        },
+
+        _onBindingChange(){
+            // No data for the binding
+            if (!this.getView().getBindingContext("employees")){
+                this.getRouter().getTargets().display("notFound");
+            }
+        }
+    });
+
+    return Employee;
+
+})
--- webapp/controller/employee/EmployeeList.controller.js
+++ webapp/controller/employee/EmployeeList.controller.js
@@ -4,7 +4,13 @@ sap.ui.define([
 	"use strict";
 
 	const EmployeeList = BaseController.extend("sap.ui.demo.nav.controller.employee.EmployeeList", {
-	
+        onListItemPressed(oEvent){
+            const item = oEvent.getSource();
+
+            this.getRouter().navTo('employee', {
+                employeeID: item.getBindingContext("employees").getProperty("EmployeeID")
+            })  
+        }
     });
 
     return EmployeeList;
